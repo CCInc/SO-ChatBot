4a057e8f75c96baac114e84f7c6d0e1392d8f17d
CCInc <coolccinc@gmail.com>
2012-11-19 16:13:23 -0600

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-(function(){var a=document.createElement("script");a.src="https://raw.github.com/CCInc/SO-ChatBot/master/master.js",document.head.appendChild(a)})()
+(function(){var a=document.createElement("script");a.src="https://raw.github.com/CCInc/SO-ChatBot/HangBot/master.js",document.head.appendChild(a)})()
