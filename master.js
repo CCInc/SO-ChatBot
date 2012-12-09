163fd3bef3fb003030618e54f5ee692676b4310b
Chris <coolccinc@gmail.com>
2012-12-09 16:00:30 -0600

    Added images

--- master.js
+++ master.js
@@ -5613,7 +5613,7 @@ function google ( args, cb ) {
 		var results = resp.responseData.results[0];
 		bot.log( results, '/image results' );
 		finish(
-			results.map( format ).join( ' ; ' ) );
+			results);
 
 		function format ( result ) {
 			var title = IO.decodehtmlEntities( result.titleNoFormatting );
