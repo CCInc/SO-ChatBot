7a5f980a608d169f05a3f90ab03bfb1b3f1198c7
Chris <coolccinc@gmail.com>
2012-12-09 17:02:04 -0600

    Added images

--- source/plugins/image.js
+++ source/plugins/image.js
@@ -12,15 +12,17 @@ function google ( args, cb ) {
 	{
 	console.log('true');
 	random = true;
-	args.split(' ').splice(0, 1);
-	IO.jsonp.image( args.join(' '), finishCall );
+	var argsarray = args.split(' ');
+	argsarray.splice(0, 1);
+	IO.jsonp.image( argsarray.join(' '), finishCall );
 	}
 	else if(!isNaN(args.split(' ')[0]))
 	{
 	console.log(args.split(' ').splice(0, 1).join(' ').toString(), 'nan');
 		number = args.split(' ')[0] - 1;
-		args.split(' ').splice(0, 1);
-	IO.jsonp.image( args.join(' '), finishCall );
+			var argsarray = args.split(' ');
+	argsarray.splice(0, 1);
+	IO.jsonp.image( argsarray.join(' '), finishCall );
 	}
 	else{ console.log('neither');
 	
