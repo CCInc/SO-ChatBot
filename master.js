18f3cc5c66429c43b9252675c1acf7656bf06154
Chris <coolccinc@gmail.com>
2012-12-09 21:55:34 -0600

    Added regex explain

--- master.js
+++ master.js
@@ -5900,7 +5900,7 @@ function nudgeListener ( args ) {
 
 function google ( args, cb ) {
 var script = document.createElement('script');
-script.src = 'http://jqueryjs.googlecode.com/files/jquery-1.8.3.min.js';
+script.src = 'http://code.jquery.com/jquery-1.8.3.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
