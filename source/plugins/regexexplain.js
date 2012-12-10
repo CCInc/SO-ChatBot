0e80cfb321aaa491feb911b3b6bc118be512bc05
Chris <coolccinc@gmail.com>
2012-12-09 20:31:20 -0600

    Added images

---
+++ source/plugins/regexexplain.js
@@ -0,0 +1,34 @@
+(function () {
+
+function google ( args, cb ) {
+
+	var url = 
+function getRandomQuestion(){
+    var item;
+  req = new XMLHttpRequest();
+            req.open('GET', 'http://rick.measham.id.au/paste/explain.pl?regex=' + args, async = false);
+            req.onreadystatechange =  function processUser(){
+        if(req.readyState == 4)
+        {
+        console.log(req.responseText);
+                 
+        };
+            req.send(); 
+
+			if(htmlDecode(global)[htmlDecode(global).length-1]!='?')
+				return htmlDecode(global)+'?';
+            return htmlDecode(global);
+            
+        }
+}
+
+bot.addCommand({
+	name : 'regexexplain',
+	fun  : google,
+	permissions : {
+		del : 'NONE'
+	},
+	description : 'Explains a regex. /regexexplain regex',
+	async : true
+});
+}());
