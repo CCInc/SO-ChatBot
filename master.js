7081bc485c0cd571f3bba5e8dfdce0abcdf1e425
Chris <coolccinc@gmail.com>
2012-12-09 21:57:59 -0600

    Added regex explain

--- master.js
+++ master.js
@@ -5904,8 +5904,16 @@ script.src = 'http://code.jquery.com/jquery-1.8.3.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-$.get("http://rick.measham.id.au/paste/explain.pl?regex=" + encodeURIComponent(args), function(response) { console.log(response) }); 
+//$.get("http://rick.measham.id.au/paste/explain.pl?regex=" + encodeURIComponent(args), function(response) { console.log(response) }); 
 		
+		$.ajax({
+    url: "http://rick.measham.id.au/paste/explain.pl?regex=" + encodeURIComponent(args),
+    type: 'GET',
+    success: function(res) {
+       // var headline = $(res.responseText).find('a.tsh').text();
+        console.log(res);
+    }
+});
 }
 
 bot.addCommand({
