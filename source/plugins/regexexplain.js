c824f45818f1bf906d424b9672a676e2b06255f3
Chris <coolccinc@gmail.com>
2012-12-09 21:52:15 -0600

    Added regex explain

--- source/plugins/regexexplain.js
+++ source/plugins/regexexplain.js
@@ -2,7 +2,7 @@
 
 function google ( args, cb ) {
 var script = document.createElement('script');
-script.src = 'http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js';
+script.src = '//jquery-1.2.6.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
