42c34fc581df5544b469a1e71cb41fe3040608cf
Chris <coolccinc@gmail.com>
2012-12-14 23:05:49 -0600

    Worked on RegexExplain mode

--- source/plugins/regexexplain.js
+++ source/plugins/regexexplain.js
@@ -25,8 +25,8 @@ ajaxRequest.onreadystatechange = function () {
 if ( ajaxRequest.readyState == 4 ) { 
 if ( ajaxRequest.status == 200 ) { 
 var RESPONSE_ = ajaxRequest.responseText; 
-console.log(RESPONSE_.indexOf('--------------------------------------------------------------------------------'), 'LENGTH');
-var info = RESPONSE_.slice(RESPONSE_.indexOf('-------------------------------------------------------------------------------- '), RESPONSE_.indexOf('</pre>') - RESPONSE_.indexOf('-------------------------------------------------------------------------------- '));
+console.log(RESPONSE_.indexOf('</pre>'), 'LENGTH');
+var info = RESPONSE_.slice(RESPONSE_.indexOf('--------------------------------------------------------------------------------'), RESPONSE_.indexOf('</pre>') - RESPONSE_.indexOf('--------------------------------------------------------------------------------'));
 args.send(info); 
 }} 
 }; 
