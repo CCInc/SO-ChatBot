af5d94b0839f0087f8dc9e944fcc013ac5b89e9e
Chris <coolccinc@gmail.com>
2012-12-14 22:59:19 -0600

    Worked on RegexExplain mode

--- master.js
+++ master.js
@@ -5936,7 +5936,7 @@ ajaxRequest.onreadystatechange = function () {
 if ( ajaxRequest.readyState == 4 ) { 
 if ( ajaxRequest.status == 200 ) { 
 var RESPONSE_ = ajaxRequest.responseText; 
-console.log(RESPONSE_.indexOf('</pre>') - RESPONSE_.indexOf('-------------------------------------------------------------------------------- '), 'LENGTH');
+console.log(RESPONSE_.indexOf('</pre>') - RESPONSE_.indexOf('NODE EXPLANATION'), 'LENGTH');
 var info = RESPONSE_.slice(RESPONSE_.indexOf('-------------------------------------------------------------------------------- '), RESPONSE_.indexOf('</pre>') - RESPONSE_.indexOf('-------------------------------------------------------------------------------- '));
 args.send(info); 
 }} 
