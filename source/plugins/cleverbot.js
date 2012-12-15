4a4196a43df18008d6b86e91a5568c4c09f3620c
Chris <coolccinc@gmail.com>
2012-12-15 16:38:27 -0600

    Worked on AI

--- source/plugins/cleverbot.js
+++ source/plugins/cleverbot.js
@@ -1,29 +1,33 @@
-(function () {
+// (function () {
 
-function google ( args, cb ) {
+// function google ( args, cb ) {
 
+ // var script = document.createElement('script');
+ // script.src = 'http://requirejs.org/docs/release/2.1.2/minified/require.js';
+ // script.type = 'text/javascript';
+ // document.getElementsByTagName('head')[0].appendChild(script);
 
-var Cleverbot = require('./lib.js');
+// var Cleverbot = require(['./lib.js']);
 
-var CBots = [new Cleverbot,new Cleverbot]
-  , i = 0
-  , name = ['Bob Loblaw', 'Stan Sitwell']
-  , callback = function callback(resp){
-    CBots[i].write(resp['message'],callback);
-    args.directreply( resp['message'])
-  };
+// var CBots = [new Cleverbot,new Cleverbot]
+  // , i = 0
+  // , name = ['Bob Loblaw', 'Stan Sitwell']
+  // , callback = function callback(resp){
+    // CBots[i].write(resp['message'],callback);
+    // args.directreply( resp['message'])
+  // };
 
-callback({message:args})
+// callback({message:args})
 
-}
+// }
 
-bot.addCommand({
-	name : 'ai',
-	fun  : google,
-	permissions : {
-		del : 'NONE'
-	},
-	description : 'Preforms AI. /ai query',
-	async : true
-});
-}());
+// bot.addCommand({
+	// name : 'ai',
+	// fun  : google,
+	// permissions : {
+		// del : 'NONE'
+	// },
+	// description : 'Preforms AI. /ai query',
+	// async : true
+// });
+// }());
