5549e550dab6695117e1ec401f3ced7870323af6
Chris <coolccinc@gmail.com>
2012-12-16 17:03:25 -0600

    Worked on AI

--- source/plugins/cleverbot.js
+++ source/plugins/cleverbot.js
@@ -53,7 +53,7 @@ var helper = function() {
                 u.push(x + "=" + encodeURIComponent(a1[x].join(",")));
             }
             else if (a1[x] instanceof Object) {
-                u.push(Cleverbot.default_params(a1[x]));
+                u.push(Cleverbot.default_params[a1[x]]);
             }
             else {
                 u.push(x + "=" + encodeURIComponent(a1[x]));
