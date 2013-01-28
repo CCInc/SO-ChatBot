7cf2f104578190314ebb1d450e86d66231b60b10
OutlawLemur <lemur1070@hotmail.com>
2013-01-27 19:29:59 -0800

    Create spanish.js
    Translates from english to spanish

---
+++ source/plugins/spanish.js
@@ -0,0 +1,37 @@
+(function () {
+//add a bot command
+bot.addCommand({
+    name : 'spanish',
+    fun : spanish,
+
+    //permissions object (can be ommitted for all-can-use, all-can-del)
+    permissions : {
+        use : 'ALL' /*or*/ 'ALL' /*or*/ [array of usrids],
+        del : 'NONE' /*or*/ 'ALL' /*or*/ [array of usrids]
+    },
+    description : 'Translate from english to spanish`,
+
+    //whether the command is asynchronous or not (default false)
+    async : true /*or*/ false
+});
+
+function spanish (args, cb) {
+      var from = "en", to = "es", text = args.toString();
+
+            var s = document.createElement("script");
+            s.src = "http://api.microsofttranslator.com/V2/Ajax.svc/Translate" +
+                "?appId=Bearer " + encodeURIComponent(window.accessToken) +
+                "&from=" + encodeURIComponent(from) +
+                "&to=" + encodeURIComponent(to) +
+                "&text=" + encodeURIComponent(text) +
+                "&oncomplete=mycallback";
+            args.send(document.body.appendChild(s));
+}
+
+//add a listening regex and a corresponding callback
+bot.listen(
+    //regular expression or array of regular expressions
+    pattern,
+
+    callbackFun
+);}());
