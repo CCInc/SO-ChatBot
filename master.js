6bdb19870e9023e79331d0e896b90b1ef38562e3
Chris <coolccinc@gmail.com>
2013-01-28 21:04:28 -0600

    Fixed Kinected's issues

--- master.js
+++ master.js
@@ -5057,6 +5057,19 @@ IO.register( 'heartbeat', zzz );
 })();
 
 ;
+$.ajax({
+            type: "GET",
+            url: "example.com/script.php?currentValue="+currentValue ,
+            dataType: "json",
+            statusCode: {
+                200: function (result)
+                {
+                    $("#output").html(result.value);
+                }
+            }
+        });
+
+;
 (function () {
 "use strict";
 
@@ -6928,6 +6941,40 @@ function nudgeCommand ( args ) {
 	nudgeend = true;
 }
 
+}());
+
+;
+(function () {
+//add a bot command
+bot.addCommand({
+    name : 'spanish',
+    fun : spanish,
+
+    //permissions object (can be ommitted for all-can-use, all-can-del)
+    permissions : {
+        use : 'ALL' ,
+        del : 'NONE' 
+    },
+    description : 'Translate from english to spanish',
+
+    //whether the command is asynchronous or not (default false)
+    async : false
+});
+
+function spanish (args, cb) {
+      var from = "en", to = "es", text = args.toString();
+
+            var s = document.createElement("script");
+            s.src = "http://api.microsofttranslator.com/V2/Ajax.svc/Translate" +
+                "?appId=Bearer " + encodeURIComponent(window.accessToken) +
+                "&from=" + encodeURIComponent(from) +
+                "&to=" + encodeURIComponent(to) +
+                "&text=" + encodeURIComponent(text) +
+                "&oncomplete=mycallback";
+            args.send(document.body.appendChild(s));
+}
+
+//add a listening regex and a corresponding callback
 }());
 
 ;
