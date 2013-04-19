e46015fbd1145e11652104d8919a6f1674870528
Chris <coolccinc@gmail.com>
2013-04-18 20:33:13 -0500

    added time lookup feature

--- master.js
+++ master.js
@@ -8845,6 +8845,64 @@ function get_matching_message ( re, onlyBefore ) {
 
 ;
 (function () {
+
+function google ( args, cb ) {
+		IO.jsonp({
+		url : 'http://www.worldweatheronline.com/feed/tz.ashx',
+		data : {
+			q   : args.toString(),
+			format: 'json',
+			callback: done,
+			key : 'e7qnb5e3xh3cy2kysypbmuj5'
+		},
+		fun : done
+	});
+
+	function done ( resp ) {
+
+		args.send(calcTime(args.toString(),resp.data.time_zone.utcOffset));
+	}
+	
+	}
+	
+	
+	// function to calculate local time
+// in a different city
+// given the city's UTC offset
+function calcTime(city, offset) {
+
+    // create Date object for current location
+    d = new Date();
+    
+    // convert to msec
+    // add local time zone offset 
+    // get UTC time in msec
+    utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    
+    // create new Date object for different city
+    // using supplied offset
+    nd = new Date(utc + (3600000*offset));
+    
+    // return time as a string
+    return "The local time in " + city + " is " + nd.toLocaleString();
+
+}
+
+
+
+bot.addCommand({
+	name : 'time',
+	fun  : google,
+	permissions : {
+		del : 'NONE'
+	},
+	description : 'Gets the current time in wherever. Acceptable formats are:- City Name; City Name, State (US only); City Name, State, Country; City Name, Country `/time request`',
+	async : true
+});
+}());
+
+;
+(function () {
 var timers = Object.create( null ),
 	id = 0;
 
